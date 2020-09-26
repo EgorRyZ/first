bf9363c876f124194aad3205fc2c9dd6f90bd1f0
EgorRyZ <70338375+EgorRyZ@users.noreply.github.com>
2020-09-26 20:22:59 +0300

    Add files via upload

--- quadratic.c
+++ quadratic.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 #include <assert.h>
+#define Epsilon 0.000001
 
-int FindRoots(double a, double b, double c, double* x1, double* x2);
+int QuadrRoots(double a, double b, double c, double* x1, double* x2);
 
 int Linear(double b, double c, double* x);
 
-int Equal(double x, double y);
+int areEqual(double x, double y);
 
 int main()
 {
@@ -15,7 +16,7 @@ int main()
 	double a = 0, b = 0, c = 0;
 	scanf("%lg %lg %lg", &a, &b, &c);
 	double x1 = 0, x2 = 0;
-	int number = FindRoots(a, b, c, &x1, &x2);
+	int number = QuadrRoots(a, b, c, &x1, &x2);
 	switch (number)
 	{
 	case 2:
@@ -36,49 +37,47 @@ int main()
 	}
 	return 0;
 }
-int FindRoots(double a, double b, double c, double* x1, double* x2)
+int QuadrRoots(double a, double b, double c, double* x1, double* x2)
 {
 	assert(x1 != NULL && x2 != NULL);
 	double D = b * b - 4 * a * c;
-	if (!Equal(a, 0)) 
+	if (!areEqual(a, 0)) 
 	{
 		if (D >= 0)
 		{
 			double sqrtD = sqrt(D);
 			*x1 = (-b - sqrtD) / 2 / a;
 			*x2 = (-b + sqrtD) / 2 / a;
-			if (Equal(D, 0))
+			if (areEqual(D, 0))
+			{
 				return 1;
-			else
-				return 2;
+			}
+			return 2;
 		}
-		else
-			return 0;		
+		return 0;		
 	}
-	else
-		return Linear(b, c, x1);
+	return Linear(b, c, x1);
 }
 int Linear(double b, double c, double* x)
 {
 	assert(x != NULL);
-	if (Equal(b, 0))
+	if (areEqual(b, 0))
 	{
-		if (Equal(c, 0))
+		if (areEqual(c, 0))
+		{
 			return -1;
-		else
-			return 0;
-	}
-	else 
-	{
-		*x = -c / b;
-		return 1;
-	}
-}
-int Equal(double x, double y)
-{
-	double e = 0.000001;
-	if (fabs(x - y) < e)
-		return 1;
-	else
+		}
 		return 0;
+	}	
+	*x = -c / b;
+	return 1;
+
+}
+int areEqual(double x, double y)
+{
+	if (fabs(x - y) < Epsilon)
+	{
+		return 1;
+	}
+	return 0;
 }
