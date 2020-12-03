685c2e61b92a6bca5d2c577918ec03ca2dd94362
EgorRyZ <70338375+EgorRyZ@users.noreply.github.com>
2020-12-03 15:36:21 +0300

    Add files via upload

--- onegin.c
+++ onegin.c
@@ -2,6 +2,7 @@
 #include <locale.h>
 #include <wchar.h>
 #include <assert.h>
+#include <stdlib.h>
 
 enum ReturnCodes
 {
@@ -10,34 +11,42 @@ enum ReturnCodes
 
 int NumOfLines(char *path);
 
-void Sort(char *path, int lineNumber[], int numOfLines);
+int MaxLength(char *path);
 
-void Swap(int *a, int *b);
+void ReadText(char *path, wchar_t **symbol);
 
-void FindStr(FILE *file, int strnum);
+void StrSort(wchar_t **symbol, int numOfLines);
 
-void OriginalOrder(int lineNumber[], int numOfLines);
+void StrSwap(wchar_t **str1, wchar_t **str2);
 
-void WriteText(char *path1, char *path2, int lineNumber[], int numOfLines);
+void WriteText(char *path, wchar_t **symbol, int numOfLines);
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	char *path1 = "C://C/c/yevgeniy-onegin.txt";
+	
+	char *path1 = "C://C/yevgeniy-onegin.txt";
 	char *path2 = "C://C/sorted.txt";
+	
 	
 	int numOfLines = NumOfLines(path1);
 	
-	int lineNumber[numOfLines];
-
-	OriginalOrder(lineNumber, numOfLines);
+	int maxLength = MaxLength(path1);
 	
-	Sort(path1, lineNumber, numOfLines);
+	wchar_t **symbol = (wchar_t**)malloc(sizeof(wchar_t*) * numOfLines);
 	
-	WriteText(path1, path2, lineNumber, numOfLines);
+	for(int i = 0; i < numOfLines; i++)
+		symbol[i] = (wchar_t*)malloc(sizeof(wchar_t) * maxLength);
+	
+	ReadText(path1, symbol);
+	
+	StrSort(symbol, numOfLines);
+	
+	WriteText(path2, symbol, numOfLines);
 
 	return 0;
 }
+
 int NumOfLines(char *path)
 {
 	FILE *file;
@@ -54,85 +63,100 @@ int NumOfLines(char *path)
 	return n;
 }
 
-void OriginalOrder(int lineNumber[], int numOfLines)
+int MaxLength(char *path)
 {
-	assert(lineNumber != NULL);
-	for(int i = 0; i < numOfLines; i++)
-	{
-		lineNumber[i] = i;
-	}
-}
-
-void FindStr(FILE *file, int strnum)
-{
+	FILE *file;
+	file = fopen(path, "r");
 	assert(file != NULL);
 	int c;
+	int max = 0;
 	int n = 0;
 	while((c = getc(file)) != EOF)
 	{
-		if(c == '\n')
-			n++;
-		if(n == strnum)
-			break;
-	}
-}
-
-void Swap(int *a, int *b)
-{
-	int t = *a;
-	*a = *b;
-	*b = t;	
-}
-
-void Sort(char *path, int lineNumber[], int numOfLines)
-{
-	assert(fopen(path, "r") != NULL);
-	assert(lineNumber != NULL);
-	for(int k = numOfLines - 1; k > 0; k--)
-	{
-		for(int j = numOfLines - 1; j > numOfLines - 1 - k; j--)
+		if(c != '\n')
+        	n++;
+		else
 		{
-			FILE *file1;
-			file1 = fopen(path, "r");
-			FILE *file2;
-			file2 = fopen(path, "r");
-			FindStr(file1, lineNumber[j - 1]);
-			FindStr(file2, lineNumber[j]);
-			int c1, c2;
-			while((c1 = getc(file1)) != EOF && (c2 = getc(file2)) != EOF)
-			{
-				if(c1 > c2 || c2 == '\n')
-				{
-					Swap(&lineNumber[j - 1], &lineNumber[j]);
-					break;
-				}
-				if(c1 < c2 || c1 == '\n')
-				{
-					break;
-				}
-			}
-			fclose(file1);
-			fclose(file2);
+			if(n > max)
+				max = n;
+			n = 0;
 		}
 	}
+	fclose(file);
+	return max;
 }
 
-void WriteText(char *path1, char *path2, int lineNumber[], int numOfLines)
+void ReadText(char *path, wchar_t **symbol)
 {
-	assert(fopen(path1, "r") != NULL);
-	assert(lineNumber != NULL);
-	int c;
-	FILE *file2;
-	file2 = fopen(path2, "a");
+	FILE *file;
+	file = fopen(path, "r");
+	assert(file != NULL);
+	int c, i = 0, j = 0;
+	while((c = getc(file)) != EOF)
+	{
+		*(*(symbol + i) + j) = c;
+		j++;
+		if(c == '\n')
+		{
+			i++;
+			j = 0;
+		}
+	}
+	fclose(file);
+}
+
+void StrSort(wchar_t **symbol, int numOfLines)
+{
+	assert(symbol != NULL);
+	for(int i = 0; i < numOfLines - 1; i++)
+	{
+		for(int j = 1; j < numOfLines - i; j++)
+		{
+			int k = 0;
+			while(1)
+			{	
+				if(*(*(symbol + j - 1) + k) > *(*(symbol + j) + k) || *(*(symbol + j) + k) == '\n')
+				{
+					StrSwap(symbol + j - 1, symbol + j);
+					break;
+				}
+				if(*(*(symbol + j - 1) + k) < *(*(symbol + j) + k) || *(*(symbol + j - 1) + k) == '\n')
+				{
+					break;
+				}
+				k++;
+			}
+		}
+	}	
+}
+
+void StrSwap(wchar_t **str1, wchar_t **str2)
+{
+	wchar_t *s = *str1;
+	*str1 = *str2;
+	*str2 = s;
+}
+
+void WriteText(char *path, wchar_t **symbol, int numOfLines)
+{
+	assert(symbol != NULL);
+	FILE *file;
+	file = fopen(path, "a");
 	for(int i = 0; i < numOfLines; i++)
 	{
-		FILE *file1;
-		file1 = fopen(path1, "r");
-		FindStr(file1, lineNumber[i]);
-		while((c = getc(file1)) != '\n')
-			putc(c, file2);
-		putc('\n', file2);
-		fclose(file1);
-	}	
-	fclose(file2);
+		int c, j = 0;
+		while((c = *(*(symbol + i) + j)) != '\n')
+		{
+			putc(c, file);
+			j++;
+		}
+		putc('\n', file);
+	}
+	fclose(file);
 }
+
+
+
+
+
+
